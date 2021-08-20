69e893f8c39a3814fecfb79d0d563533e7f3b205
wehoi <89265553+wehoi@users.noreply.github.com>
2021-08-20 22:56:51 +0700

    Add files via upload

--- restore.sh
+++ restore.sh
@@ -3,6 +3,8 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
+echo "Processing"
+clear
 echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
 echo "Please input link to your vps data backup file."
 echo "You can check it on your email if you run backup data vps before."
