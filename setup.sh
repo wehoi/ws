11dcd76af6cf516137361e696de9aee335447111
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-03 03:58:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,24 +5,12 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://github.com/wehoi/ws/raw/main/cf.sh && chmod +x cf.sh && ./cf.sh
-#install ssh ovpn
+#รันโอเพ่นวีพีเอน
 wget https://github.com/wehoi/ws/raw/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-#install ssr
-wget https://github.com/wehoi/ws/raw/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://github.com/wehoi/ws/raw/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#installwg
-wget https://github.com/wehoi/ws/raw/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
-#install v2ray
+#รันวีทูเรย์
 wget https://github.com/wehoi/ws/raw/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
-wget https://github.com/wehoi/ws/raw/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-
 rm -f /root/ssh-vpn.sh
-rm -f /root/wg.sh
-rm -f /root/ss.sh
-rm -f /root/ssr.sh
 rm -f /root/ins-vt.sh
-rm -f /root/set-br.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
@@ -46,7 +34,7 @@ clear
 echo " "
 echo " installation completed"
 echo " "
-echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "=================================-พรีเมี่ยมสคริป-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -58,15 +46,11 @@ echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
-echo "   - Wireguard               : 7070"  | tee -a log-install.txt
-echo "   - Shadowsocks-R           : 8443-9543"  | tee -a log-install.txt
-echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
-echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
 echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
-echo "   - Trojan                  : 443"  | tee -a log-install.txt
+echo "   - Trojan                  : 2443"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Bangkok (GMT +7)"  | tee -a log-install.txt
@@ -84,11 +68,10 @@ echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo ""
-echo "   - Owner                   : YOGA CANDRA KIRANA" | tee -a log-install.txt
-echo "   - Telegram                : @Kamatari"  | tee -a log-install.txt
-echo "   - Whatsapp                : +6285600174618"  | tee -a log-install.txt
+echo "   - Owner                   : เอเจ" | tee -a log-install.txt
+echo "   - LINE                    : aj07"  | tee -a log-install.txt
 echo ""
-echo "--------------------------Script By YOGA CANDRA KIRANA ------------------------" | tee -a log-install.txt
+echo "--------------------------สคริปโดยเอเจ ------------------------" | tee -a log-install.txt
 echo ""
 echo " type "roboot" to reboot your server "  | tee -a log-install.txt
 sleep 10
