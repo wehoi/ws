fcc68beaf13914f99bf609e3c30c5d9fb909e516
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-06 12:27:29 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://github.com/javakeisha/my-new/raw/main/vpn.zip
+wget https://github.com/wehoi/ws/raw/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
