9366cf14bc773ea059768ae92288729d179c20b5
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-12 19:28:26 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -5,22 +5,22 @@ NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 clear
 echo -e ""
-echo -e "======================================"
+echo -e "*************************************"
 echo -e ""
 echo -e ""
-echo -e "    [1] Restart All Services"
-echo -e "    [2] Restart OpenSSH"
-echo -e "    [3] Restart Dropbear"
-echo -e "    [4] Restart Stunnel4"
-echo -e "    [5] Restart OpenVPN"
-echo -e "    [6] Restart Squid"
-echo -e "    [7] Restart Nginx"
-echo -e "    [8] Restart Badvpn"
-echo -e "    [x] Exit"
+echo -e "    [1] รีสตาร์ท​ทั้งหมด"
+echo -e "    [2] รีสตาร์ท​ OpenSSH"
+echo -e "    [3] รีสตาร์ท​ Dropbear"
+echo -e "    [4] รีสตาร์ท​ Stunnel4"
+echo -e "    [5] รีสตาร์ท​ OpenVPN"
+echo -e "    [6] รีสตาร์ท​ Squid"
+echo -e "    [7] รีสตาร์ท​ Nginx"
+echo -e "    [8] รีสตาร์ท​ Badvpn"
+echo -e "    [x] ออก"
 echo -e ""
-read -p "    Select From Options [1-8 or x] :  " Restart
+read -p "    เลือกจาก [1-8 หรือ x] :  " Restart
 echo -e ""
-echo -e "======================================"
+echo -e "**************************************"
 sleep 1
 clear
 case $Restart in
@@ -38,77 +38,77 @@ case $Restart in
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "          Service/s Restarted         "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 2)
                 clear
                 /etc/init.d/ssh restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "         SSH Service Restarted        "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 3)
                 clear
                 /etc/init.d/dropbear restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "       Dropbear Service Restarted     "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 4)
                 clear
                 /etc/init.d/stunnel4 restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "        Stunnel4 Service Restarted    "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 5)
                 clear
                 /etc/init.d/openvpn restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "       OpenVPN Service Restarted      "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 6)
                 clear
                 /etc/init.d/squid restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "        Squid3 Service Restarted      "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 7)
                 clear
                 /etc/init.d/nginx restart
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "         Nginx Service Restarted      "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 8)
@@ -123,11 +123,11 @@ case $Restart in
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7800 --max-clients 500
                 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7900 --max-clients 500
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 echo -e ""
                 echo -e "    Badvpn  Badvpn Service Restarted  "
                 echo -e ""
-                echo -e "======================================"
+                echo -e "**************************************"
                 exit
                 ;;
                 x)
