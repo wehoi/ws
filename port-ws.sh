cf0a8e4334d531a0cb0d4e56431d53364dcc609a
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-12 18:36:14 +0700

    Update port-ws.sh

--- port-ws.sh
+++ port-ws.sh
@@ -6,12 +6,12 @@ MYIP=$(wget -qO- icanhazip.com);
 clear
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
-echo -e "======================================"
+echo -e "***************************************"
 echo -e ""
 echo -e "     [1]  เปลี่ยนพอร์ต Vmess TLS $tls"
 echo -e "     [2]  เปลี่ยนพอร์ต Vmess None TLS $none"
 echo -e "     [x]  ออก"
-echo -e "======================================"
+echo -e "***************************************"
 echo -e ""
 read -p "     เลือกจาก [1-2 หรือ x] :  " prot
 echo -e ""
