016dfccf8e7df4b6c3e2b71b9bcea2eafe8a6f8f
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-13 22:57:53 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
+echo "กำลังดำเนินการ"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
