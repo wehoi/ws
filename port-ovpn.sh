f23939c970fa54fd51024539e2e330ccbb1ac3a9
wehoi <89265553+wehoi@users.noreply.github.com>
2021-09-14 17:27:51 +0700

    Update port-ovpn.sh

--- port-ovpn.sh
+++ port-ovpn.sh
@@ -75,7 +75,7 @@ systemctl disable --now openvpn-server@server-tcp-$ovpn > /dev/null
 systemctl enable --now openvpn-server@server-tcp-$vpn > /dev/null
 sed -i "s/   - OpenVPN                 : TCP $ovpn, UDP $ovpn2, SSL 442/   - OpenVPN                 : TCP $vpn, UDP $ovpn2, SSL 442/g" /root/log-install.txt
 sed -i "s/$ovpn/$vpn/g" /etc/stunnel/stunnel.conf
-echo -e "\e[032;1mพอร์ต $vpn แก้ไขสำหรับแล้ว\e[0m"
+echo -e "\e[032;1mพอร์ต $vpn แก้ไขสำเร็จแล้ว\e[0m"
 else
 echo "พอร์ต $vpn ถูกใช้งานแล้ว"
 fi
