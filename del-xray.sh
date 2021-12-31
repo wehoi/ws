1f4d929f614ed95c9af1bddc7364ed061989068e
wehoi <89265553+wehoi@users.noreply.github.com>
2022-01-01 05:05:25 +0700

    Update and rename del-xray.txt to del-xray.sh

--- del-xray.sh
+++ del-xray.sh
@@ -2,16 +2,16 @@
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray-mini/vless-direct.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
-		echo "You have no existing clients!"
+		echo "ไม่มีบัญชีในระบบ!"
 		exit 1
 	fi
 
 	clear
 	echo ""
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
-	echo " ==============================="
-	echo "     No  Expired   User"
+	echo " เลือกบัญชีที่ต้องการลบ"
+	echo " พิมพ์​ CTRL+C เพื่อย้อนกลับ"
+	echo " *************************"
+	echo " ไม่มีบัญชีที่หมดอายุ"
 	grep -E "^### " "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
@@ -38,8 +38,9 @@ systemctl restart xray-mini@vless-direct
 systemctl restart xray-mini@vless-splice
 
 clear
-echo " XRay Account Deleted Successfully"
-echo " =========================="
-echo " Client Name : $user"
-echo " Expired On  : $exp"
-echo " =========================="
+echo " ลบบัญชีเรียบร้อยแล้ว"
+echo " **************************"
+echo " ชื่อ          : $user"
+echo " วันหมดอายุ​    : $exp"
+echo " **************************"
+echo " สคริปโดยเอเจ" 
