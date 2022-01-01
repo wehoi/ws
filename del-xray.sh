5d7ab124e266a2378e88af13541e29cd979e5843
wehoi <89265553+wehoi@users.noreply.github.com>
2022-01-01 08:06:03 +0700

    Update del-xray.sh

--- del-xray.sh
+++ del-xray.sh
@@ -1,46 +1,32 @@
 #!/bin/bash
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray-mini/vless-direct.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo ""
-		echo "ไม่มีบัญชีในระบบ!"
-		exit 1
-	fi
-
-	clear
-	echo ""
-	echo " เลือกบัญชีที่ต้องการลบ"
-	echo " พิมพ์​ CTRL+C เพื่อย้อนกลับ"
-	echo " *************************"
-	echo " ไม่มีบัญชีที่หมดอายุ"
-	grep -E "^### " "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
-user=$(grep -E "^### " "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/xray-mini/vless-direct.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/vless-direct.json
-sed -i "/^### $user $exp/,/^},{/d" /etc/xray-mini/vless-splice.json
-
-# // Restarting Service
-systemctl stop xray-mini@vless-direct
-systemctl stop xray-mini@vless-splice
-systemctl disable xray-mini@vless-direct
-systemctl disable xray-mini@vless-splice
-systemctl enable xray-mini@vless-direct
-systemctl enable xray-mini@vless-splice
-systemctl start xray-mini@vless-direct
-systemctl start xray-mini@vless-splice
-systemctl restart xray-mini@vless-direct
-systemctl restart xray-mini@vless-splice
-
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
 clear
-echo " ลบบัญชีเรียบร้อยแล้ว"
-echo " **************************"
-echo " ชื่อ          : $user"
-echo " วันหมดอายุ​    : $exp"
-echo " **************************"
-echo " สคริปโดยเอเจ" 
+	clear
+	echo -e "Memadam User Xray"
+	echo -e "-----------------"
+	read -p "Username : " user
+	echo -e ""
+	if ! grep -qw "$user" /kaizen/xray/xray-clients.txt; then
+		echo -e ""
+		echo -e "User '$user' does not exist."
+		echo -e ""
+		exit 0
+	fi
+	uuid="$(cat /kaizen/xray/xray-clients.txt | grep -w "$user" | awk '{print $2}')"
+
+	cat /usr/local/etc/xray/config.json | jq 'del(.inbounds[0].settings.clients[] | select(.id == "'${uuid}'"))' > /usr/local/etc/xray/config_tmp.json
+	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
+	cat /usr/local/etc/xray/config.json | jq 'del(.inbounds[1].settings.clients[] | select(.id == "'${uuid}'"))' > /usr/local/etc/xray/config_tmp.json
+	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
+	sed -i "/\b$user\b/d" /kaizen/xray/xray-clients.txt
+	service xray restart
+clear
+echo -e ""
+echo " ================================"
+echo " Akaun Xray Vless berjaya dipadam"
+echo " ================================"
+echo -e ""
