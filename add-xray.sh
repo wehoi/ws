cdb7526335071fd0757b971c6d3516f3db041fcf
wehoi <89265553+wehoi@users.noreply.github.com>
2022-01-01 08:04:52 +0700

    Update add-xray.sh

--- add-xray.sh
+++ add-xray.sh
@@ -1,66 +1,52 @@
 #!/bin/bash
-source /var/lib/premium-script/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/v2ray/domain)
-else
-domain=$IP
-fi
-
-# // Vless
-vless1="$( cat /etc/xray-mini/vless-direct.json | grep -w port | awk '{print $2}' | sed 's/,//g' )"
-vless2="$( cat /etc/xray-mini/vless-splice.json | grep -w port | awk '{print $2}' | sed 's/,//g' )"
-
-until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
-		read -rp "ชื่อ: " -e user
-		CLIENT_EXISTS=$(grep -w $user /etc/xray-mini/vless-direct.json | wc -l)
-
-		if [[ ${CLIENT_EXISTS} == '1' ]]; then
-			echo ""
-			echo "มีชื่อในระบบแล้ว โปรดเลือกชื่ออื่น."
-			exit 1
-		fi
-	done
-
-uuid=$(cat /proc/sys/kernel/random/uuid)
-read -p "จำนวนวันใช้งาน: " masaaktif
-exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-
-# // Input To Server
-sed -i '/#XRay$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","flow": "xtls-rprx-direct","email": "'""$user""'"' /etc/xray-mini/vless-direct.json
-sed -i '/#XRay$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","flow": "xtls-rprx-splice","email": "'""$user""'"' /etc/xray-mini/vless-splice.json
-
-vless_direct="vless://${uuid}@${domain}:${vless1}?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct&sni=bug#$user"
-vless_splice="vless://${uuid}@${domain}:${vless1}?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-splice&sni=bug#$user"
-
-# // Restarting Service
-systemctl stop xray-mini@vless-direct
-systemctl stop xray-mini@vless-splice
-systemctl disable xray-mini@vless-direct
-systemctl disable xray-mini@vless-splice
-systemctl enable xray-mini@vless-direct
-systemctl enable xray-mini@vless-splice
-systemctl start xray-mini@vless-direct
-systemctl start xray-mini@vless-splice
-systemctl restart xray-mini@vless-direct
-systemctl restart xray-mini@vless-splice
-
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
 clear
-echo -e ""
-echo -e "*********************************"
-echo -e "          ข้อมูลบัญชี"
-echo -e "ชื่อ​             : ${user}"
-echo -e "โดเมน​           : ${domain}"
-echo -e "พอร์ต Direct    : $vless1"
-echo -e "พอร์ต Splice    : $vless1"
-echo -e "ไอดี​            : ${uuid}"
-echo -e "เส้นทาง          : /"
-echo -e "วันหมดอายุ​       : $exp"
-echo -e "*********************************"
-echo -e "ลิงก์ Direct     : ${vless_direct}"
-echo -e "*********************************"
-echo -e "ลิงก์ Splice     : ${vless_splice}"
-echo -e "*********************************"
-echo -e "สคริปโดยเอเจ" 
+source /var/lib/premium-script/ipvps.conf
 
+	clear
+  	echo -e ""
+	echo -e "Menambah User Xray"
+	echo -e "------------------"
+	read -p "Username : " user
+	if grep -qw "$user" /kaizen/xray/xray-clients.txt; then
+		echo -e ""
+		echo -e "User '$user' already exist."
+		echo -e ""
+		exit 0
+	fi
+	read -p "Duration (day) : " duration
+	
+	uuid=$(cat /proc/sys/kernel/random/uuid)
+	exp=$(date -d +${duration}days +%Y-%m-%d)
+	expired=$(date -d "${exp}" +"%d %b %Y")
+	domain=$(cat /etc/v2ray/domain)
+	email=${user}@${domain}
+	echo -e "${user}\t${uuid}\t${exp}" >> /kaizen/xray/xray-clients.txt
+	cat /usr/local/etc/xray/config.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","flow": "xtls-rprx-direct","email": "'${email}'"}]' > /usr/local/etc/xray/config_tmp.json
+	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
+	cat /usr/local/etc/xray/config.json | jq '.inbounds[1].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /usr/local/etc/xray/config_tmp.json
+	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
+	service xray restart
+	
+	clear
+echo -e "===========-XRAY/VLESS-=========="
+echo -e "Remarks        : ${user}"
+echo -e "IP             : ${MYIP}"
+echo -e "Port XTLS      : 443"
+echo -e "id             : ${uuid}"
+echo -e "Encryption     : none"
+echo -e "Flow           : xtls-rprx-direct"
+echo -e "Network        : tcp"
+echo -e "Security       : xtls"
+echo -e "Allow Insecure : enable"
+echo -e "================================="
+echo -e "XRAY XTLS      : vless://$uuid@$domain:443?security=xtls&flow=xtls-rprx-direct#vless_xtls_${user}"
+echo -e "================================="
+echo -e "QR Code Link   : https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=vless://$uuid@$domain:443?security=xtls&flow=xtls-rprx-direct#vless_xtls_${user}"
+echo -e "================================="
+echo -e "Expired On     : $expired"
+echo -e ""
